5b64b583fd941ce2499b92ef6424b37695ad0ed0
Rene Ventura <venturar729@gmail.com>
2018-12-18 19:42:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -10,3 +10,15 @@ def prime?(number)
   end
   true
 end
+
+def prime_two?(number)
+  if number <= 1
+    return false
+  end
+  (2..number).each do |num|
+    if number % num == 0
+      return false
+    end
+  end
+  true
+end
